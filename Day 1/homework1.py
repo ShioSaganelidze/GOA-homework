880bc65497b2598080930c153fad0ae77e9789b2
ShioSaganelidze <158043308+ShioSaganelidze@users.noreply.github.com>
2024-02-18 17:47:04 +0400

    Add files via upload

---
+++ Day 1/homework1.py
@@ -0,0 +1,26 @@
+print("ნინო კობახიძე")
+print("შიო საგანელიძე")
+print("კოკა საგანელიძე")
+print("გიორგი საგანელიძე")
+print("ნიკოლოზ საგანელიძე")
+
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
+print 2
